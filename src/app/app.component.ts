c7913247fb71aa25ae14c6a91e69f4b63b98953e
i.le.ka <zaliibragimova@yandex.ru>
2022-11-13 09:56:41 +0300

    Added the first version of the interface

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'untitled';
+  namePlan = '';
+
+  plans: {num: number, name: string, rang: number}[] = [
+    {num: 1, name: 'Поднятие корпоративного духа', rang: 3},
+    {num: 2, name: 'Покупка нового ПО', rang: 2},
+    {num: 3, name: 'Покупка нового оборудования', rang: 1},
+    {num: 4, name: 'Расширение сервера', rang: 4},
+  ];
+
+  isRangCalc = true;
+
+  addPlan(): void {
+    this.plans.push({num: this.plans.length + 1, name: this.namePlan, rang: 0});
+    this.namePlan = '';
+  }
 }
